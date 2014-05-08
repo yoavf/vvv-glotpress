be5294e1504051d6aa375c7c4d0b5c469416b558
Marko Heijnen <info@markoheijnen.com>
2014-05-08 08:14:17 +0200

    Create database and config file for unit tests.

--- vvv-init.sh
+++ vvv-init.sh
@@ -5,14 +5,21 @@ echo "Commencing VVV GlotPress Setup"
 # Make a database, if we don't already have one
 echo "Creating database (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS glotpress"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS glotpress_test"
 mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON glotpress.* TO gp@localhost IDENTIFIED BY 'gp';"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON glotpress_test.* TO gp@localhost IDENTIFIED BY 'gp';"
 
 # Download GlotPress
 if [ ! -d htdocs ]
 then
 	echo "Checking out GlotPress SVN"
 	svn checkout http://glotpress.svn.wordpress.org/trunk/ htdocs
+
 	cp htdocs/gp-config-sample.php htdocs/gp-config.php
+	sed -i 's/username/gp/g' htdocs/gp-config.php
+	sed -i 's/password/gp/g' htdocs/gp-config.php
+
+	cp htdocs/t/unittests-config-sample.php htdocs/t/unittests-config.php
 	sed -i 's/username/gp/g' htdocs/gp-config.php
 	sed -i 's/password/gp/g' htdocs/gp-config.php
 else
